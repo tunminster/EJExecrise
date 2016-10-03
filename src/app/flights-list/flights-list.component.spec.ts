a85722fc5ed1cea1144be21382abd5994fe75180
Tun <tunminster@hotmail.co.uk>
2016-10-03 23:15:01 +0100

    added flight list folder

---
+++ src/app/flights-list/flights-list.component.spec.ts
@@ -0,0 +1,11 @@
+/* tslint:disable:no-unused-variable */
+
+import { TestBed, async } from '@angular/core/testing';
+import { FlightsListComponent } from './flights-list.component';
+
+describe('Component: FlightsList', () => {
+  it('should create an instance', () => {
+    let component = new FlightsListComponent();
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/flights-list/flights-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-flights-list',
+  templateUrl: './flights-list.component.html',
+  styleUrls: ['./flights-list.component.css']
+})
+export class FlightsListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
